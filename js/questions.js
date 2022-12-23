24e9c7569970fd88aff08eeda28fbce7cd8d229d
kacperlechicki <klechicki@poczta.fm>
2022-12-23 23:33:01 +0100

    first five of questions

---
+++ js/questions.js
@@ -0,0 +1,44 @@
+'use strict';
+
+let questions = [
+	{
+		question: 'Who painted the Mona Lisa?',
+		numb: 1,
+		options: [
+			'Vincent van Gogh',
+			'Pablo Picasso',
+			'Leonardo da Vinci',
+			'Claude Monet',
+		],
+		answer: 'Leonardo da Vinci',
+	},
+	{
+		question: 'Where was the mojito cocktail created?',
+		numb: 2,
+		options: ['Cuba', 'Spain', 'Jamaica', 'Finland'],
+		answer: 'Cuba',
+	},
+	{
+		question: 'How many bones does a shark have?',
+		numb: 3,
+		options: ['13', '2', '4', '0'],
+		answer: '0',
+	},
+	{
+		question: "What is Scooby Doo's full name",
+		numb: 4,
+		options: [
+			'Scooby van Dooby',
+			'Scoobert Doo',
+			'Scoobert Doobert',
+			'Scarby Darby',
+		],
+		answer: 'Scoobert Doo',
+	},
+	{
+		question: 'What is the smallest country in the world?',
+		numb: 5,
+		options: ['Monaco', 'Russia', 'Nauru', 'Vatican City'],
+		answer: 'Vatican City',
+	},
+];
