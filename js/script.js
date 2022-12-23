2c15e24f0faedb7d9c0c22c659c28e275c6b18da
kacperlechicki <klechicki@poczta.fm>
2022-12-23 07:30:50 +0100

    first question

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-"use strict";
+'use strict';
+
+// buttons
+const startQuizButton = document.querySelector('.start-btn');
+const quitButton = document.querySelector('.quit');
+
+// boxes/forms
+const infoBox = document.querySelector('.info');
+
+//show info
+const startQuiz = () => {
+	infoBox.style.display = 'block';
+	startQuizButton.style.display = 'none';
+};
+
+//quit to start button
+const quitQuiz = () => {
+	infoBox.style.display = 'none';
+	startQuizButton.style.display = 'block';
+};
+
+startQuizButton.addEventListener('click', startQuiz);
+quitButton.addEventListener('click', quitQuiz);
