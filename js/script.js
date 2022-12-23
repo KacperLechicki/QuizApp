11d90f22ab154c9f77b07b317f972f146f08b7b5
kacperlechicki <klechicki@poczta.fm>
2022-12-23 09:50:05 +0100

    quiz question form

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@
 // buttons
 const startQuizButton = document.querySelector('.start-btn');
 const quitButton = document.querySelector('.quit');
+const continueButton = document.querySelector('.continue');
 
 // boxes/forms
 const infoBox = document.querySelector('.info');
+const quizBox = document.querySelector('.quiz-box');
 
 //show info
 const startQuiz = () => {
@@ -19,5 +21,11 @@ const quitQuiz = () => {
 	startQuizButton.style.display = 'block';
 };
 
+const playQuiz = () => {
+	quizBox.style.display = 'block';
+	infoBox.style.display = 'none';
+};
+
 startQuizButton.addEventListener('click', startQuiz);
 quitButton.addEventListener('click', quitQuiz);
+continueButton.addEventListener('click', playQuiz);
