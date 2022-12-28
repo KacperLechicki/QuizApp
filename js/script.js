ca77dac7e1d947bf2525b2c7b0c2bfbc46eb040d
kacperlechicki <klechicki@poczta.fm>
2022-12-28 14:30:52 +0100

    show options for current question

--- js/script.js
+++ js/script.js
@@ -4,14 +4,18 @@
 const startQuizButton = document.querySelector('.start-btn');
 const quitButton = document.querySelector('.quit');
 const continueButton = document.querySelector('.continue');
+const nextButton = document.querySelector('.next');
 
 // boxes/forms
 const infoBox = document.querySelector('.info');
 const quizBox = document.querySelector('.quiz-box');
 
+// lets
+let questions_count = 0;
+
 //show info
 const startQuiz = () => {
-	infoBox.classList.add('activeInfo')
+	infoBox.classList.add('activeInfo');
 };
 
 //quit to start button
@@ -23,6 +27,22 @@ const quitQuiz = () => {
 const playQuiz = () => {
 	quizBox.classList.add('activeQuiz');
 	infoBox.classList.remove('activeInfo');
+	showQuestions(2);
+};
+
+//getting questions and options from array
+const showQuestions = (index) => {
+	const questionText = document.querySelector('.question-text');
+	const optionList = document.querySelector('.option-list');
+	let questionTag = `<p class="question">${questions[index].question}</p>`;
+	let optionTag =
+		`<div class="option"><p class="option-text">${questions[index].options[0]}</p></div>` +
+		`<div class="option"><p class="option-text">${questions[index].options[1]}</p></div>` +
+		`<div class="option"><p class="option-text">${questions[index].options[2]}</p></div>` +
+		`<div class="option"><p class="option-text">${questions[index].options[3]}</p></div>`;
+
+	questionText.innerHTML = questionTag;
+	optionList.innerHTML = optionTag;
 };
 
 startQuizButton.addEventListener('click', startQuiz);
