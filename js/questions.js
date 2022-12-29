0e0a23194fb03c295a61b6e9f98deca56bee5fa2
kacperlechicki <klechicki@poczta.fm>
2022-12-30 00:01:56 +0100

    more questions

--- js/questions.js
+++ js/questions.js
@@ -41,4 +41,36 @@ let questions = [
 		options: ['Monaco', 'Russia', 'Nauru', 'Vatican City'],
 		answer: 'Vatican City',
 	},
+	{
+		question: 'What is the smallest planet in our solar system?',
+		numb: 6,
+		options: ['Earth', 'Mercury', 'Wenus', 'Mars'],
+		answer: 'Mercury',
+	},
+	{
+		question: 'What is the currency of Denmark?',
+		numb: 7,
+		options: ['Krone', 'USD', 'EUR', 'PLN'],
+		answer: 'Krone',
+	},
+	{
+		question:
+			'Which popular video game franchise has released games with the subtitles World At War and Black Ops?',
+		numb: 8,
+		options: ['Sniper Elite', 'Mortal Kombat', 'Battlefield', 'Call of duty'],
+		answer: 'Call of duty',
+	},
+	{
+		question:
+			'How many films have Al Pacino and Robert De Niro appeared in together?',
+		numb: 9,
+		options: ['1', '7', '2', '4'],
+		answer: '4',
+	},
+	{
+		question: 'Name the Coffee shop in US sitcom Friends.',
+		numb: 10,
+		options: ['Kaffe', 'Just coffee', 'Central Perk', 'Molly'],
+		answer: 'Central Perk',
+	},
 ];
